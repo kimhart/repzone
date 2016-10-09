5c966be009e7d68b8a418fbc5816f5e826bff94b
alexhubbard89 <alexhubbard89@gmail.com>
2016-10-09 14:34:31 -0400

    python zip code working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
     PythonShell = require('python-shell'),
     sqli = require('sqli'), 
     sqlite = sqli.getDriver('sqlite'),
-    conn = sqlite.connect('db/rep_zone.db');
+    conn = sqlite.connect('rep_zone.db');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -23,20 +23,35 @@ app.set('view engine', 'ejs');
 
 // Python Script //
 
-// PythonShell.run('my_script.py', function (err) {
-//   if (err) throw err;
-//   console.log('Ran python script');
-// });
 
+//start.js
+var spawn = require('child_process').spawn,
+    py    = spawn('python', ['python/get_senator_by_zip.py']),
+    data = 11238,
+    dataString = '';
 
-// Routes //
+py.stdout.on('data', function(data){
+  dataString += data.toString();
+});
+
+py.stdout.on('end', function(){
+  console.log(dataString.replace('\n', ''));
+});
+
+py.stdin.write(JSON.stringify(data));
 
 app.get('/', function(req, res){
-  // res.render('index.ejs');
-  conn.exec('SELECT * FROM current_rep_bio WHERE state = "NY"').each(function(row){
+  conn.exec('SELECT  * FROM current_rep_bio WHERE state ="' + 
+    dataString.replace('\n', '') + '"').all(function(row){
     res.json(row);
-  });
+    /*console.log(row)*/
+  })
 });
+
+
+py.stdin.end();
+
+
 
 app.listen(process.env.port || 3000);
 
---
+++ python/get_senator_by_zip.py
@@ -0,0 +1,40 @@
+#Read data from stdin
+def read_in():
+    import sys, json, numpy as np
+    lines = sys.stdin.readlines()
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def get_senator_by_zip():
+    ## Find your senator by zipcode. 
+    ## Because there are only two senators 
+    ## and entire states are represented by
+    ## those two senetors, I only need to find
+    ## the state from the zip code and then
+    ## locate the current senator.
+
+    import requests
+    import pandas as pd
+    from pandas.io.json import json_normalize
+    
+    ## Get representatives from zip code.
+    zip_code = read_in()
+
+
+    url = 'http://maps.googleapis.com/maps/api/geocode/json?address={}&sensor=true'.format(zip_code)
+    r = requests.get(url)
+    x = pd.DataFrame(json_normalize(r.json()['results']).loc[0, 'address_components']).loc[:,]
+    for i in range(len(x['types'])):
+        if u'administrative_area_level_1' in x.loc[i, 'types']:
+    ## Save state from zipcode
+            state = x.loc[i, 'short_name'].upper()
+
+    #print state
+    #print state
+    print state
+
+
+
+#start process
+if __name__ == '__main__':
+    get_senator_by_zip()
